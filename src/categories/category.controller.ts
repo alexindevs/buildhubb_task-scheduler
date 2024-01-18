85738e67641c29b647232482614b479d7fddc2e5
Alexin <alexindevs@gmail.com>
2024-01-18 23:26:40 +0100

    fix oop error

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -1,16 +1,13 @@
 import CategoryService from './category.service';
 import { Request, Response } from 'express';
 
-export default class CategoryController {
-    categoryService: CategoryService;
-    constructor() {
-        this.categoryService = new CategoryService();
-    }
+const categoryService = new CategoryService();
 
+export default class CategoryController {
     async createCategory(req: Request, res: Response) {
        try {
             const { name, color, id } = req.body;
-            const category = await this.categoryService.createCategory(name, color, id);
+            const category = await categoryService.createCategory(name, color, id);
             return res.status(201).json(category);
         } catch (error: any) {
             console.log(error);
@@ -21,7 +18,7 @@ export default class CategoryController {
     async getCategoryByUserId(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const categories = await this.categoryService.getCategoriesByUser(id);
+            const categories = await categoryService.getCategoriesByUser(id);
             return res.status(200).json(categories);
         } catch (error: any) {
             console.log(error);
@@ -32,7 +29,7 @@ export default class CategoryController {
     async getCategoryById(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const category = await this.categoryService.getCategoryById(id);
+            const category = await categoryService.getCategoryById(id);
             return res.status(200).json(category);
         } catch (error: any) {
             console.log(error);
@@ -44,7 +41,7 @@ export default class CategoryController {
         try {
             const { id } = req.params;
             const { name, color } = req.body;
-            const category = await this.categoryService.updateCategory(id, { name, color });
+            const category = await categoryService.updateCategory(id, { name, color });
             return res.status(200).json(category);
         } catch (error: any) {
             console.log(error);
@@ -55,7 +52,7 @@ export default class CategoryController {
     async deleteCategory(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const category = await this.categoryService.deleteCategory(id);
+            const category = await categoryService.deleteCategory(id);
             return res.status(200).json(category);
         } catch (error: any) {
             console.log(error);
